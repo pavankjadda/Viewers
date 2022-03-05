0c2c07ada4119741617dedbe5b41f64bec7b6171
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-03-04 19:00:56 -0500

    feat(jwt): sets gcp jwt token in angular app and consumes in this app

--- platform/core/src/DICOMWeb/getAuthorizationHeader.js
+++ platform/core/src/DICOMWeb/getAuthorizationHeader.js
@@ -29,7 +29,7 @@ export default function getAuthorizationHeader({ requestOptions } = {}) {
   else if (accessToken) {
     headers.Authorization = `Bearer ${accessToken}`;
   }
-
-  headers.Authorization = `Bearer eyJraWQiOiJiZWNlNDY4ZmI4ZWFmMjEzZDU4YjJiZTFhMmU3NTE4M2QxZWRiNGZhIiwiYWxnIjoiUlMyNTYifQ.eyJhdWQiOiJodHRwczovL2hlYWx0aGNhcmUuZ29vZ2xlYXBpcy5jb20vIiwiaXNzIjoibWliLWFpci1zZXJ2aWNlLWFjY291bnRAbmloLW5jaS1jY3ItZGljb20tc2JveC0zNGFkLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoibWliLWFpci1zZXJ2aWNlLWFjY291bnRAbmloLW5jaS1jY3ItZGljb20tc2JveC0zNGFkLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwiaWF0IjoxNjQ1NzU2ODM1LCJleHAiOjE2NDU3NTc0MzV9.Qhvfl77cRBqVBDFUv1E1l7XmeGeMDTkRoRaqbtPajZEANf57bDAD2iu-v2fhlmc02N1EIy_x7U4ItRxB5KjWbhZTGLf6gZKX9zHpRGJR9zCpwk3wUfUsCfxNPQD_vT0-JAlHvatYXhjW_Tm_KeWz9A0R8Ioktfb3Jr3E3pEqVgGmGdJ8wdkjuzM6twJowZ4-X7eeOzI_M_Iaka5_1YjERI4mL0Dh6xhpM-4NYdSsrafGWtYUhtilOc9koDoOD4E7Wg7GBC3jzVriEmpI-TK5IG5vb0FmxB0JQZCVpYNaYNV_4X9RKYhbduoXaskuo2qT7cMiVJUyEXlJqcJNLQq4lA`;
+  const gcpJwtToken = localStorage.getItem('gcp-jwt-token');
+  headers.Authorization = `Bearer ${gcpJwtToken}`;
   return headers;
 }
